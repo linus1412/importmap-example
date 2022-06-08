c3b2c684afc8c416176350e11fe8b78170f500db
Martin Smith <martin@smitek.co.uk>
2022-06-08 22:20:48 +0100

    Add .gitignore

--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@ import startCase from '@lodash/startCase';
 const el = document.createElement('h1');
 const words = "hello, world";
 const text = document.createTextNode(startCase(words) + " " + new Date().toTimeString());
-el.appendChild(text);   
+el.appendChild(text);
 
 document.body.appendChild(el);
