b0b8e7cdfc4a5a8c7b14d6378e78bcb1e54214ed
Martin Smith <martin@smitek.co.uk>
2022-06-08 22:18:57 +0100

    Initial commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+import startCase from '@lodash/startCase';
+
+const el = document.createElement('h1');
+const words = "hello, world";
+const text = document.createTextNode(startCase(words) + " " + new Date().toTimeString());
+el.appendChild(text);   
+
+document.body.appendChild(el);
